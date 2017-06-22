3a8bd1fd6b32c4145a057fbb46341be0248e4de7
RomanHalaburda <mtbrider94@gmail.com>
2017-06-23 02:40:57 +0300

    Start

---
+++ RepublicDatabaseAutoservice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RepublicDatabaseAutoservice.Global" Language="C#" %>
---
+++ RepublicDatabaseAutoservice/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RepublicDatabaseAutoservice.Startup))]
+namespace RepublicDatabaseAutoservice
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
